d2a4c6abfe2f41facc715206270ff2424a5671b6
Hakan Kayan <kayanh@cardiff.ac.uk>
2021-02-21 18:04:52 +0000

    Add new file

---
+++ Arduino/temp_wifi_code/temp_wifi_code.ino
@@ -0,0 +1,51 @@
+`
+/*Please replace with your own correct variables if you see "!R" near the given variable.*/
+#include "DHT.h"
+
+#define DHTPIN 4 //!R
+#define DHTTYPE DHT11 //!R
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long pass_time = millis();
+int exit_while = 0;
+
+void setup()
+{
+    /*Initialize ports*/
+    Serial1.begin(115200);
+    Serial.begin(115200);
+
+    /*Set-up Wi-Fi Module*/
+    delay(1000);
+    Serial1.print("AT\\r\\n");
+    delay(500);
+    Serial1.print("AT+CWMODE_DEF=3\\r\\n");
+    delay(500);
+    Serial1.print("AT+CWJAP=\"Work\",\"GroveWork\"\\r\\n"); //!R
+    delay(500);
+}
+
+void loop()
+{
+    float temp_hum_val[2] = {0};
+    char recvChar;
+    unsigned long pass_time2 = millis();
+    `+ debug_part + `
+    /*Connect to Host*/
+    if(millis() - pass_time > 20000 && exit_while == 0){
+        Serial1.print("AT+CIPSTART=\"TCP\",\"192.168.43.28\",4448\\r\\n"); //!R
+        exit_while = 1;
+    }
+
+    /*Send Data to Host*/
+    if(!dht.readTempAndHumidity(temp_hum_val) && pass_time2 > 25000 ){
+        String temp_data = String(temp_hum_val[1],2);
+        int total_bytes = temp_data.length()+16; //!R make sure its right.
+        String total_bytes_sent = String(total_bytes);
+        Serial1.print("AT+CIPSEND="+total_bytes_sent+"\\r\\n");
+        delay(1000);
+        Serial1.print("`+ id_loc + `"+";"+"` + textbox_nodeID + `"+";"+"` + id_temp + `"+":"+temp_data+":0;"+"` + id_wifi +`"+"\\n");
+    }
+    delay(`+ textbox + `); //Desired delay
+}
+`
