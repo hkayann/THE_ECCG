7e3e89b080ff8c9043dd3d1f26376e4c44c5916f
hkayann <75617629+hkayann@users.noreply.github.com>
2021-03-10 19:23:40 +0000

    Update temp_hum_wifi_code_advanced.ino

--- Arduino/temp_hum_wifi_code_advanced/temp_hum_wifi_code_advanced.ino
+++ Arduino/temp_hum_wifi_code_advanced/temp_hum_wifi_code_advanced.ino
@@ -5,7 +5,8 @@
 #define DHTPIN 4 //!R
 #define DHTTYPE DHT11 //!R
 DHT dht(DHTPIN, DHTTYPE);
-
+//send data per interval seconds(end user decides)
+unsigned long interval =`+textbox_timeInterval+`L;
 unsigned long pass_time = millis();
 int exit_while = 0;
 
@@ -33,7 +34,7 @@ void loop()
   float temp_hum_val[2] = {0};
   char recvChar;
   byte i = 0;
-    `+ debug_part +
+    `+ debug_part +`
   /*Connect to Host*/
   if(millis() - pass_time > 20000 && exit_while == 0){
     Serial1.print("AT+CIPSTART=\"TCP\",\"`+ textbox_hostip + `\",` + textbox_port +`\\r\\n");
