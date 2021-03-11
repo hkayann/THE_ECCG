b5f9b392225fdc49c30d59416a5115988ce68633
hkayann <75617629+hkayann@users.noreply.github.com>
2021-03-11 12:18:30 +0000

    Update hum_air_zigbee_code_advanced.ino

--- Arduino/hum_air_zigbee_code_advanced/hum_air_zigbee_code_advanced.ino
+++ Arduino/hum_air_zigbee_code_advanced/hum_air_zigbee_code_advanced.ino
@@ -7,7 +7,7 @@
 AirQualitySensor sensor(A2); //!R
 DHT dht(DHTPIN, DHTTYPE);
 //send data per interval seconds(end user decides)
-unsigned long interval = 30000L;
+unsigned long interval =`+textbox_timeInterval+`L;
 
 void setup() {
   // put your setup code here, to run once:
