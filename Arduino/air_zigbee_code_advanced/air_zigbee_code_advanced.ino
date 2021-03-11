b51a481f192a0ce6f06640c566330ad2ff7aec6e
hkayann <75617629+hkayann@users.noreply.github.com>
2021-03-11 12:33:50 +0000

    Update air_zigbee_code_advanced.ino

--- Arduino/air_zigbee_code_advanced/air_zigbee_code_advanced.ino
+++ Arduino/air_zigbee_code_advanced/air_zigbee_code_advanced.ino
@@ -3,7 +3,8 @@
 #include "Air_Quality_Sensor.h"
 
 AirQualitySensor sensor(A2); //!R
-
+//send data per interval seconds(end user decides)
+unsigned long interval =`+textbox_timeInterval+`L;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
